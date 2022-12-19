9451a9c4c11f2f8023492c3bdf065e8d9ddc0f76
Jobwawesh <jobnjunge15@gmail.com>
2022-12-20 01:00:08 +0300

    made the login disappear

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
-function hideLoginForm() {
-    document.getElementById('login-form').style.display = 'none';
-    document.getElementById('login-button').addEventListener('click', hideLoginForm);
-  }
+const loginForm = document.getElementById('login-form')
+const header = document.getElementById('header')
+const mainDetails = document.getElementById('main')
+
+function hideElems(){
+    loginForm.style.display = "none"
+    // header.style.display = 'none'
+}
+
+//This function submits our login form
+loginForm.addEventListener("submit", function onsubmit(e) {
+    e.preventDefault()
+
+    hideElems();
+    
+    mainDetails.style.display= "block";
+    loginForm.reset()
+})
