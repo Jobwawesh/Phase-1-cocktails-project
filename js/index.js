f47fba82c28f5d6860dfd319f275da9ca1ebfcd5
Jobwawesh <jobnjunge15@gmail.com>
2022-12-20 08:28:20 +0300

    added random drinks

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const loginForm = document.getElementById('login-form')
 const header = document.getElementById('header')
 const mainDetails = document.getElementById('main')
 const abtEl = document.getElementById('Aboutinfo')
+const abouttag = document.getElementById("abttag")
+
 
 function hideElems(){
     loginForm.style.display = "none"
@@ -24,4 +26,130 @@ abouttag.addEventListener("click", () => {
   mainDetails.style.display= "none";
   hideEl();
 })
+
+//Fetched URLS
+//1. Random drinks
+const randomDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+
+//2. cattegories
+const CATEGORIES = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail'
+
+
+document.addEventListener('DOMContentLoaded',()=> {
+
+  //Data
+  const randomDrinkRow = document.getElementById('main-container')
+  const categoriesLink = document.getElementById('category-link')
+  const drinksCategoryRow = document.getElementById('drinks-category')
+
+
+  // CLICK EVENTS FOR LINKS
+  categoriesLink.addEventListener('click', () => {
+    // hide random meal
+    randomDrinkRow.style.display = "none"
+    
+    drinksCategoryRow.removeAttribute('hidden')
+    drinksCategoryRow.style.display = "flex"
+
+})
+
+
+  
+
+  //create an element for the random drink
+  const createRandomDrink = (image, name, description) => {
+    const cardDiv = document.createElement('div')
+    cardDiv.classList.add('card', 'col')
+
+    const rowDiv = document.createElement('div')
+    rowDiv.classList.add('row')
+
+    const imgDiv = document.createElement('div')
+    imgDiv.classList.add('col')
+
+    const bodyDiv = document.createElement('div')
+    bodyDiv.classList.add('card-col')
+
+    const drinkImg = document.createElement('img')
+    drinkImg.classList.add('card-img')
+    drinkImg.src = image
+
+    const drinkTitle = document.createElement('h3')
+    drinkTitle.classList.add('card-title')
+    drinkTitle.innerText = name
+
+    const drinkDescription = document.createElement('p')
+    drinkDescription.classList.add('card-text')
+    drinkDescription.innerText = description
+
+    //append body elements
+    bodyDiv.appendChild(drinkTitle)
+    bodyDiv.appendChild(drinkDescription)
+
+    //append image elements
+    imgDiv.appendChild(drinkImg)
+
+    //append divs to row
+    rowDiv.appendChild(imgDiv)
+    rowDiv.appendChild(bodyDiv)
+
+    //append row to card
+    cardDiv.appendChild(rowDiv)
+
+    //return the cardDiv
+    return cardDiv
+  }
+
+  const loadRandomDrink = () => {
+    fetch(randomDrinks)
+    .then((response) => response.json())
+    .then((data) => {
+        const drinkData = data.drinks[[0]]
+        const name = drinkData.strDrink
+        const description = drinkData.strInstructions
+        const image = drinkData.strDrinkThumb
+        const drinkElement = createRandomDrink(image, name, description)
+        randomDrinkRow.appendChild(drinkElement)
+    })
+  }
+
+  //create category element
+  const createCategory = (image, name) =>{
+    const cardDiv = document.createElement('div')
+    cardDiv.classList.add('card', 'col-4')
+
+    const categoryImg = document.createElement('img')
+    cardDiv.classList.add('card-img-top')
+    categoryImg.src = image
+
+    const categoryTitle = document.createElement('h4')
+    categoryTitle.classList.add('card-title')
+    categoryTitle.innerText = name
+
+
+    //append title and image to card
+    cardDiv.appendChild(categoryImg)
+    cardDiv.appendChild(categoryTitle)
+
+    return cardDiv
+    
+}
+
+//load drink categories
+const loadCategories = () => {
+    fetch(CATEGORIES)
+    .then((response) => response.json())
+    .then((data) => {
+        const categoriesData = data.drinks
+        const categoryElems  = categoriesData.map(
+            cat => createCategory(cat.strDrinkThumb, cat.strDrink)
+        )
+        drinksCategoryRow.append(...categoryElems)
+    })
+}
+  loadRandomDrink()
+  loadCategories()
+
+})
+
 
