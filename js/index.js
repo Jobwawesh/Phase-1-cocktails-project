ea35c1f1d6a838392638e3e694b11d2ff086fba5
Jobwawesh <jobnjunge15@gmail.com>
2022-12-20 23:58:21 +0300

    fixed typos

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@ document.addEventListener('DOMContentLoaded',()=> {
   })
 
   //This function links our About tag in the navigation menu to the About page
-  abouttag.addEventListener("click", (abtEl) => {
+  abouttag.addEventListener("click", () => {
     //hide this elements
     randomDrinkRow.style.display = "none"
     mainDetails.style.display= "none";
@@ -206,5 +206,4 @@ const initCategories = () => {
 }
   initRandomDrink()
   initCategories()
-
 })
